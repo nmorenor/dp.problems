c7521f790817b3b28be07cc94bcf44c62b8d27f6
Ignacio Moreno <ignacio@genuitec.com>
2020-10-28 09:55:53 -0600

    Problem impl

--- src/com/nmorenor/dp/problems/TheMesseuse.java
+++ src/com/nmorenor/dp/problems/TheMesseuse.java
@@ -9,9 +9,30 @@ package com.nmorenor.dp.problems;
  * overlap, and none can be moved), find the optimal (highest total booked
  * minutes) set the messeuse can honor. Return the number of minutes
  * 
+ * Example:
+ * input: 30, 15, 60, 75, 45, 15, 15, 45
+ * Output: 60 minutes 30. 60, 45, 45
+ * 
  * @author nacho
  *
  */
 public class TheMesseuse {
 
+	public static void main(String[] args) {
+		int[] input = new int[] {30, 15, 60, 75, 45, 15, 15, 45};
+		System.out.println(maxMinutes(input, 0, new int[input.length]));
+	}
+	
+	private static int maxMinutes(int[] massages, int index, int[] memo) {
+		if (index >= massages.length) {
+			return 0;
+		}
+		if (memo[index] == 0) {
+			int bestWith = massages[index] + maxMinutes(massages, index + 2, memo);
+			int bestWithout = massages[index] + maxMinutes(massages, index + 1, memo);
+			memo[index] = Math.max(bestWith, bestWithout);
+		}
+		
+		return memo[index];
+	}
 }
